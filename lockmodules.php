787c475bd35694062d138ce82b58d1005829e973
dangertwo <dhoffman19@live.com>
2014-04-12 23:09:52 -0400

    lock module fix
    Now won't stop room page from displaying all the pis.

--- lockmodules.php
+++ lockmodules.php
@@ -42,13 +42,13 @@ setInterval(update, 1000);
 <?php
 
 
-  $query = "Select * from lock_m where M_ID =" . $lockId;
+  $querylock = "Select * from lock_m where M_ID =" . $lockId;
 
-  $results = mysqli_query($link, $query);
+  $resultslock = mysqli_query($link, $querylock);
 
-  $ar = mysqli_fetch_array($results);
+  $arlock = mysqli_fetch_array($resultslock);
  
-  $yesno = $ar['state'];
+  $yesno = $arlock['state'];
 
   if($yesno) {
     echo '<td><h5>Lock '.$lockId.'</h5></td><td><button type="button" id="on'.$lockId.'" class="btn btn-danger" onclick="changeState'.$lockId.'()">Unlock</button> </td>
